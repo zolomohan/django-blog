b0ce5b1fbd526780993b67c779e981c665222533
Mohan Raj <mohanzolomon@gmail.com>
2019-11-28 12:34:39 +0530

    Add base.html to Blog Templates

--- src/blog/views.py
+++ src/blog/views.py
@@ -7,4 +7,4 @@ def home(request):
     return render(request, 'blog/home.html', {})
 
 def about(request):
-    return render(request, 'blog/about.html', {})
+    return render(request, 'blog/about.html', {'title': 'About'})
