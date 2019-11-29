26d04624f54f169f25dbbbac59a2ff3e1eeedb17
Mohan Raj <mohanzolomon@gmail.com>
2019-11-29 10:54:00 +0530

    Add Crispy Forms

--- src/django_project/settings.py
+++ src/django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party Apps
+    'crispy_forms',
+    # User Apps
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig'
 ]
@@ -120,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
