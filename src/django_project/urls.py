72a5345027972d78fe10608013de44a107b5dcf2
Mohan Raj <mohanzolomon@gmail.com>
2019-11-29 10:14:17 +0530

    Add Register Route

--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users.views import register
 
 urlpatterns = [
+    path('register/', register),
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
 ]
