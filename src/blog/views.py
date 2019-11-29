abfefba8610bf475bde54f7670ab0aebfc04280c
Mohan Raj <mohanzolomon@gmail.com>
2019-11-29 09:22:30 +0530

    Add Post Model

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-  {
-    'title': 'Post 1',
-    'author': 'John Doe',
-    'date_posted': '28th November 2019',
-    'content': 'Non mollit sint magna dolore sint aute sunt. Occaecat amet commodo minim occaecat tempor esse nulla quis minim. Eiusmod enim sit enim elit velit excepteur et veniam elit nostrud ad est laboris pariatur. Ex officia dolore non dolore officia cupidatat qui cupidatat eu sunt.'
-  },
-  {
-    'title': 'Post 2',
-    'author': 'Jack Doe',
-    'date_posted': '27th November 2019',
-    'content': 'Non mollit sint magna dolore sint aute sunt. Occaecat amet commodo minim occaecat tempor esse nulla quis minim. Eiusmod enim sit enim elit velit excepteur et veniam elit nostrud ad est laboris pariatur. Ex officia dolore non dolore officia cupidatat qui cupidatat eu sunt.'
-  }
-]
+from .models import Post
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'blog/home.html', {'posts': Post.objects.all()})
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
