f3c0529dbbe846eed85b9d8f482c2915b112c659
Mohan Raj <mohanzolomon@gmail.com>
2019-11-29 14:18:01 +0530

    Add User Profile Route

--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from users.views import register
+from users.views import register, profile
 
 urlpatterns = [
     path('register/', register, name='user-register'),
+    path('profile/', profile, name='user-profile'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='user-login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
     path('admin/', admin.site.urls),
--- src/users/views.py
+++ src/users/views.py
@@ -17,3 +17,6 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'users/profile.html')
