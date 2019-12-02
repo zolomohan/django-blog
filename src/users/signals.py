a43f98cf63672b5648b3fe5901a92ee2dc12ffb6
Mohan Raj <mohanzolomon@gmail.com>
2019-12-02 10:38:50 +0530

    Fix Typo

--- src/users/signals.py
+++ src/users/signals.py
@@ -10,4 +10,4 @@ def create_user(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user(sender, instance, **kwargs):
-    instance.Profile.save()
+    instance.profile.save()
